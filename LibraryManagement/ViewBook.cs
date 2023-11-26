7d335e4bb5fd50ba10f145ec606d29523da058a8
Vathanak <vathanaksol1605@gmail.com>
2023-11-26 10:54:23 +0700

    Finish Part 6

--- LibraryManagement/ViewBook.cs
+++ LibraryManagement/ViewBook.cs
@@ -25,6 +25,7 @@ namespace LibraryManagement
 
         private void ViewBook_Load(object sender, EventArgs e)
         {
+            panel2.Visible = false;
             SqlConnection con = new SqlConnection();
             con.ConnectionString = "data source = DESKTOP-E4UJ6RE\\SQLEXPRESS; database = library; integrated security = true";
             SqlCommand cmd = new SqlCommand();
@@ -33,7 +34,9 @@ namespace LibraryManagement
             cmd.CommandText = "select * from NewBook";
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
-            da.Fill(ds);
+            da.Fill(ds); // Error can't find database
+
+            dataGridView1.DataSource = ds.Tables[0];
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -49,6 +52,118 @@ namespace LibraryManagement
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+        int bid;
+        Int64 rowid;
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                bid = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                //MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            }
+            panel2.Visible = true;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-E4UJ6RE\\SQLEXPRESS; database = library; integrated security = true";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = "select * from NewBook where bid= "+bid+"";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            rowid = Int64.Parse(ds.Tables[0].Rows[0][0].ToString());
+
+            txtBName.Text = ds.Tables[0].Rows[0][1].ToString();
+            txtAuthor.Text = ds.Tables[0].Rows[0][2].ToString();
+            txtPublication.Text = ds.Tables[0].Rows[0][3].ToString();
+            txtPDate.Text = ds.Tables[0].Rows[0][4].ToString();
+            txtPrice.Text = ds.Tables[0].Rows[0][5].ToString();
+            txtQuantity.Text = ds.Tables[0].Rows[0][6].ToString();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+        }
+
+        private void txtBookName_TextChanged(object sender, EventArgs e)
+        {
+            if(txtBookName.Text != "")
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source = DESKTOP-E4UJ6RE\\SQLEXPRESS; database = library; integrated security = true";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = "select * from NewBook where bName LIKE '" +txtBookName.Text+ "%'";
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds); // Error can't find database
+
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source = DESKTOP-E4UJ6RE\\SQLEXPRESS; database = library; integrated security = true";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = "select * from NewBook";
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds); // Error can't find database
+
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            txtBookName.Clear();
+            panel2.Visible = false;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Data Will Be Updated. Confirm?","Success",MessageBoxButtons.OKCancel,MessageBoxIcon.Question)==DialogResult.OK)
+            {
+                String bname = txtBName.Text;
+                String bauthor = txtAuthor.Text;
+                String publication = txtPublication.Text;
+                String pdate = txtPDate.Text;
+                Int64 price = Int64.Parse(txtPrice.Text);
+                Int64 quan = Int64.Parse(txtQuantity.Text);
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source = DESKTOP-E4UJ6RE\\SQLEXPRESS; database = library; integrated security = true";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = "update NewBook set bName = '" + bname + "',bAuthor = '" + bauthor + "',bPulb='" + publication + "',bPDate='" + pdate + "',bPrice=" + price + ",bQuan=" + quan + " where bid=" + rowid + "";
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds); // Error can't find database
+            }
+           
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Data Will Deleted. Confirm?", "Confirmation Dialog", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source = DESKTOP-E4UJ6RE\\SQLEXPRESS; database = library; integrated security = true";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = "delete from NewBook where bid=" + rowid + "";
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds); // Error can't find database
+            }
         }
     }
 }
