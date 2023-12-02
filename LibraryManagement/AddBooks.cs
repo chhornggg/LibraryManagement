ce2b8854e2a579adc405571ea3bdb314fcf5b79a
chhornggg <sengephorng@gmail.com>
2023-12-01 20:12:44 -0800

    Added OOP to AddBooks.cs Form

--- LibraryManagement/AddBooks.cs
+++ LibraryManagement/AddBooks.cs
@@ -8,9 +8,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace LibraryManagement
 {
+
     public partial class AddBooks : Form
     {
         public AddBooks()
@@ -33,16 +35,15 @@ namespace LibraryManagement
 
             if (txtBookName.Text != "" && txtAuthor.Text != "" && txtPublication.Text != "" && txtPrice.Text != "" && txtQuantity.Text != "")
             {
-
-
+                /*
                 String bname = txtBookName.Text;
                 String bauthor = txtAuthor.Text;
                 String publication = txtPublication.Text;
                 String pdate = dateTimePicker1.Text;
                 Int64 price = Int64.Parse(txtPrice.Text);
                 Int64 quan = Int64.Parse(txtQuantity.Text);
-
-                SqlConnection con = new SqlConnection();
+                */
+                /*SqlConnection con = new SqlConnection();
                 con.ConnectionString = "data source = DESKTOP-CF5N97R\\SQLEXPRESS; database = library; integrated security=True";
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = con;
@@ -53,6 +54,15 @@ namespace LibraryManagement
                 con.Close();
 
                 MessageBox.Show("Data Saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                */
+                Book newBook = new Book();
+                newBook.BookName = txtBookName.Text;
+                newBook.BookAuthor = txtAuthor.Text;
+                newBook.BookPublication = txtPublication.Text;
+                newBook.Date = dateTimePicker1.Text;
+                newBook.Price = Int64.Parse(txtPrice.Text);
+                newBook.Quantity = Int64.Parse(txtQuantity.Text);
+                newBook.AddToDatabase();
 
                 txtBookName.Clear();
                 txtAuthor.Clear();
@@ -72,7 +82,88 @@ namespace LibraryManagement
             {
                 this.Close(); 
             }
-            
+        }
+    }
+
+    public class Book
+    {
+        private String bookName;
+        private String bookAuthor;
+        private String bookPublication;
+        private String date;
+        private Int64 price;
+        private Int64 quantity;
+
+        public String BookName
+        {
+            get { return bookName; }
+            set { bookName = value; }
+        }
+
+        public String BookAuthor
+        {
+            get { return bookAuthor; }
+            set { bookAuthor = value; }
+        }
+
+        public String BookPublication
+        {
+            get { return bookPublication; }
+            set { bookPublication = value; }
+        }
+
+        public String Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public Int64 Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public Int64 Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public Book()
+        {
+            bookName = "";
+            bookAuthor = "";
+            bookPublication = "";
+            date = "";
+            price = 0;
+            quantity = 0;
+        }
+        public Book(String bookName, String bookAuthor, String bookPublication, String date, Int64 price, Int64 quantity)
+        {
+            this.bookName = bookName;
+            this.bookAuthor = bookAuthor;
+            this.bookPublication = bookPublication;
+            this.date = date;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public void AddToDatabase()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-CF5N97R\\SQLEXPRESS; database = library; integrated security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            con.Open();
+            cmd.CommandText = "insert into NewBook (bName,bAuthor,bPubl,bPDate,bPrice,bQuan) values ('" + BookName + "', '" + BookAuthor + "', '" + BookPublication + "', '" + Date + "', " + Price + ", " + Quantity + ")";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
+
+
